0fa3587d440c7af7d494775bc55d42245318c2f4
TajulMunandar <tajulmunandar701@gmail.com>
2023-07-17 09:50:09 +0700

    migration

--- app/Models/choiceUser.php
+++ app/Models/choiceUser.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class choiceUser extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function User()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function Jawaban()
+    {
+        return $this->belongsTo(jawaban::class, 'jawabanId');
+    }
 }
