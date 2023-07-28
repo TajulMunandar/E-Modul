b10816d901829994017ab8ffb57b946a2fdbddcc
alvinsyahri <alvinsyahri080@gmail.com>
2023-07-28 12:59:45 +0700

    add trigger

--- database/migrations/2023_07_28_045327_penilaian.php
+++ database/migrations/2023_07_28_045327_penilaian.php
@@ -16,7 +16,16 @@ return new class extends Migration
             CREATE TRIGGER penilaian
             AFTER INSERT ON choice_users FOR EACH ROW
             BEGIN
+                DECLARE total_soal INT;
+                DECLARE total_benar INT;
+                DECLARE nilai DECIMAL(5, 2);
 
+                SELECT COUNT(*) INTO total_soal FROM questions WHERE quizId = NEW.quizId;
+                SELECT COUNT(*) INTO total_benar FROM jawabans WHERE questionId IN (SELECT id FROM questions WHERE quizId = NEW.quizId) AND status = 1 AND id IN (SELECT jawabanId FROM choice_users WHERE userId = NEW.userId);
+                SET nilai = (total_benar / total_soal) * 100;
+
+                INSERT INTO skor (userId, quizId, nilai) VALUES (NEW.userId, NEW.quizId, nilai)
+                ON DUPLICATE KEY UPDATE nilai = nilai;
             END;
         EOT;
 
