81c67a579c7381f862eafa208bb777ec4eb42e8e
TajulMunandar <tajulmunandar701@gmail.com>
2023-07-30 14:04:17 +0700

    materi

--- app/Http/Controllers/PramateriController.php
+++ app/Http/Controllers/PramateriController.php
@@ -33,9 +33,31 @@ class PramateriController extends Controller
     public function showmateri(materi $materi)
     {
         $materis = $materi->modul->user;
+        $navmateris = materi::with('modul')->where('modulId', $materi->modulId)->get();
+        $footmateris = materi::with('modul')->where('modulId', $materi->modulId)->get();
+
+        // Mengurutkan $footmateris berdasarkan id (misalnya dari yang terkecil)
+        $footermateris = $footmateris->sortBy('id')->values();
+
+        // Mengambil indeks dari $materi dalam $footmateris
+        $currentMateriIndex = $footermateris->search(function ($item) use ($materi) {
+            return $item->id === $materi->id;
+        });
+
+        // Mengambil data materi sebelumnya (jika ada)
+        $previousMateri = null;
+        if ($currentMateriIndex > 0) {
+            $previousMateri = $footermateris[$currentMateriIndex - 1];
+        }
+
+        // Mengambil data materi sesudahnya (jika ada)
+        $nextMateri = null;
+        if ($currentMateriIndex < $footermateris->count() - 1) {
+            $nextMateri = $footermateris[$currentMateriIndex + 1];
+        }
         $tanggal = Carbon::parse($materi->created_at)->format('d F Y');
 
-        return view('main.page.materi')->with(compact('materi', 'materis', 'tanggal'));
+        return view('main.page.materi')->with(compact('materi', 'materis', 'tanggal', 'navmateris', 'previousMateri', 'nextMateri'));
     }
 
     public function showquizez(string $id)
