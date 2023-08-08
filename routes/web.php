e764840a4b5c845f2e1b9defd5d6c9844ed93ee3
TajulMunandar <tajulmunandar701@gmail.com>
2023-08-08 18:59:02 +0700

    hak akses

--- routes/web.php
+++ routes/web.php
@@ -40,41 +40,44 @@ Route::controller(LoginController::class)->group(function () {
     Route::post('/logout', 'logout');
 });
 
-Route::get('/modul', [ModulController::class, 'index'] )->name('modul-main.index');
+Route::get('/modul', [ModulController::class, 'index'])->name('modul-main.index');
 
 Route::get('/pramateri/{modul:slug}', [PramateriController::class, 'showPramateri'])->name('pramateri.show');
 
 Route::get('/pramateri/{modul:slug}/pramateri', [PramateriController::class, 'showPramateri'])->name('pramateri-main.show');
 
-Route::get('/pramateri/{modul:slug}/quiz', [PramateriController::class, 'showQuiz'])->name('pramateri-quiz.show');
+Route::get('/pramateri/{modul:slug}/quiz', [PramateriController::class, 'showQuiz'])->name('pramateri-quiz.show')->middleware('auth');
 
 Route::get('/materi-main/{materi:slug}', [PramateriController::class, 'showmateri'])->name('materi-main.show');
 Route::post('/materi-main', [PramateriController::class, 'storeMateri'])->name('materi-main.store');
 
-Route::get('/quiz-main/{id}', [QuizController::class, 'showquiz'])->name('quiz-main.showquiz');
-Route::resource('/quiz-main', QuizController::class)->middleware('auth')->except('showquiz');
+Route::get('/quiz-main/{id}', [QuizController::class, 'showquiz'])->name('quiz-main.showquiz')->middleware('mahasiswa');
+Route::resource('/quiz-main', QuizController::class)->middleware('auth')->except('showquiz')->middleware('mahasiswa');
 
-Route::post('/quiz-main-essay', [EssayController::class, 'store'])->middleware('auth')->name('quiz-essay.store');
+Route::post('/quiz-main-essay', [EssayController::class, 'store'])->middleware('auth')->name('quiz-essay.store')->middleware('mahasiswa');
 
-Route::prefix('/dashboard')->group( function (){
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+Route::prefix('/dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('admin');
     Route::resource('/category', DashboardCategoryController::class)->middleware('auth');
     Route::resource('/modul', DashboardModulController::class)->middleware('auth');
     Route::resource('/materi', DashboardMateriController::class)->middleware('auth');
-    Route::resource('/user', DashboardUserController::class)->middleware('auth');
-    Route::post('/user/reset-password', [DashboardUserController::class, 'resetPasswordAdmin'])->name('user.reset')->middleware('auth');
-    Route::prefix('/quizz')->group( function (){
-        Route::resource('/choicee', DashboardQuizzController::class)->middleware('auth');
-        Route::resource('/essayy', DashboardQuizzController::class)->middleware('auth');
-        Route::resource('/question', DashboardQuestionController::class)->middleware('auth');
+    Route::resource('/user', DashboardUserController::class)->middleware('admin');
+    Route::post('/user/reset-password', [DashboardUserController::class, 'resetPasswordAdmin'])->name('user.reset')->middleware('admin');
+    Route::middleware(['auth'])->group(function () {
+        Route::prefix('/quizz')->group(function () {
+            Route::resource('/choicee', DashboardQuizzController::class);
+            Route::resource('/essayy', DashboardQuizzController::class);
+            Route::resource('/question', DashboardQuestionController::class);
+        });
     });
-    Route::prefix('/penilaian')->group( function(){
-        Route::get('/choice', [DashboardPenilaianController::class, 'index'])->name('choice.index');
-        Route::get('/choice/{choice}', [DashboardPenilaianController::class, 'show'])->name('choice.show');
-        Route::get('/essay', [DashboardPenilaianController::class, 'index'])->name('essay.index');
-        Route::get('/essay/{essay}', [DashboardPenilaianController::class, 'show'])->name('essay.show');
-        Route::put('/essay/{essay}', [DashboardPenilaianController::class, 'update'])->name('essay.update');
-        Route::put('/essay/detail/{essay}', [DashboardPenilaianController::class, 'updateitem'])->name('essay.updateitem');
+    Route::middleware(['auth'])->group(function () {
+        Route::prefix('/penilaian')->group(function () {
+            Route::get('/choice', [DashboardPenilaianController::class, 'index'])->name('choice.index');
+            Route::get('/choice/{choice}', [DashboardPenilaianController::class, 'show'])->name('choice.show');
+            Route::get('/essay', [DashboardPenilaianController::class, 'index'])->name('essay.index');
+            Route::get('/essay/{essay}', [DashboardPenilaianController::class, 'show'])->name('essay.show');
+            Route::put('/essay/{essay}', [DashboardPenilaianController::class, 'update'])->name('essay.update');
+            Route::put('/essay/detail/{essay}', [DashboardPenilaianController::class, 'updateitem'])->name('essay.updateitem');
+        });
     });
 });
-
