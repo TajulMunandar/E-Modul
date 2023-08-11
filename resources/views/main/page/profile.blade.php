fb1d9264f2b80e59fde0f0eb939c8c2acf1785d1
TajulMunandar <tajulmunandar701@gmail.com>
2023-08-11 11:26:53 +0700

    profile image

--- resources/views/main/page/profile.blade.php
+++ resources/views/main/page/profile.blade.php
@@ -9,7 +9,7 @@
                         <div class="text-center">
                             <div class="row">
                                 <div class="col">
-                                    <a type="button" class="position-relative">
+                                    <a type="button" class="position-relative" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                         <div>
                                             @if (auth()->user()->id)
                                                 <img src="{{ asset('images/avatar.png') }}" alt="" width="25%"
@@ -26,6 +26,30 @@
                                             </span>
                                         </div>
                                     </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Change Profile Image</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="mb-3">
+                                            <label for="formFile" class="form-label">Pilih Foto Anda</label>
+                                            <input class="form-control" type="file" id="formFile">
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-info text-white">Save changes</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
